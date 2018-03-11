9a7c44dfca12495459ed87d6563c77deb6d996cb
Chris Li <hychrisli@users.noreply.github.com>
2018-03-10 21:15:10 -0800

    clear cache

--- header.php
+++ header.php
@@ -24,8 +24,3 @@
         </nav>
     </div>
 </header>
-<?php
-header("Cache-Control: no-cache, must-revalidate");
-header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
-header("Content-Type: application/xml; charset=utf-8");
-?>
